65bdad1399cab399a49e5d4a564d547c0fb1dec7
bala <balaranjan@outlook.com>
2018-06-26 22:36:32 -0500

    second commit

--- 1_ga/ga-nnp.py
+++ 1_ga/ga-nnp.py
@@ -1,5 +1,6 @@
 import math
 import random
+import operator
 from ase.lattice.cubic import FaceCenteredCubic
 from ase.build import surface
 
@@ -8,6 +9,9 @@ Developing Neural Network Potential for nanoparticles with the help of
 genetic algorithm. The NNP will be trained using training data from 
 calculations of slab and GA will evolve the set of planes that are
 required to construct the NNP for NPs
+
+GA tutorial: https://blog.sicara.com/getting-started-genetic-algorithms-python-tutorial-81ffa1dd72f9
+
 """
 
 def fitness(reference_energy, nnp_energy):
@@ -26,26 +30,37 @@ def generate_slab(miller_indices, layers):
 
     return surface(lattice, miller_indices, layers, vacuum=15)
 
-def generate_first_population(population_size):
+def generate_first_population(population_size, mi_per_individual=10):
+
+    """
+    The target is to find the best set of miller_indices that can represent the 
+    chemical environment of atoms in NP. So "an individual" of the population
+    will be composed of several (fixed to 10 for now) miller_indices.
+    """
 
     population = []
 
     while len(population) < population_size:
 
-        # Get three random intergers 0-9
-        m1 = int(random.random() * 9)
-        m2 = int(random.random() * 9)
-        m3 = int(random.random() * 9)
+        individual = []
+        while len(individual) < mi_per_individual:
+            # Get three random intergers 0-9
+            m1 = int(random.random() * 9)
+            m2 = int(random.random() * 9)
+            m3 = int(random.random() * 9)
 
-        miller_indices = [m1, m2, m3]
+            miller_indices = [m1, m2, m3]
         
-        # Make sure [0, 0, 0] is not generated!
-        if m1 == m2 == m3 == 0:
-            print("h, k, l = 0 !!!")
-            miller_indices[int(random.random() * 2)] += (int(random.random() * 8) + 1)
+            # Make sure [0, 0, 0] is not generated!
+            if m1 == m2 == m3 == 0:
+                print("h, k, l = 0 !!!")
+                miller_indices[int(random.random() * 2)] += (int(random.random() * 8) + 1)
 
-        population.append(miller_indices)
+            individual.append(miller_indices)
+        population.append(individual)
 
     return population
 
 
+def compute_population_performance(population, reference):
+    pass
